9ee3c26a7d2d64d6a4c122a4ac1a55c99903ab1b
Maxime Guillemot <guillemot.maxime@gmail.com>
2018-08-16 16:01:33 +0200

    Adding "home" and "post" pages + system to deliver the right page through index.php

---
+++ pages/home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+
+		<title>Blog - Jean Forteroche</title>
+
+		<meta name="description" content="Roman publié par billets de blog" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<meta name="application-name" content="Billet simple pour l'Alaska" />
+		
+		<meta property="og:description" content="Roman publié par billets de blog" />
+		<meta property="og:title" content="Blog - Jean Forteroche" />
+		<meta property="og:site_name" content="Billet simple pour l'Alaska" />
+		<meta property="og:type" content="website" />
+		<meta property="og:url" content="http://maximeguillemot.com/formation/projet4/" />
+		<meta property="og:image" content="" />
+		
+		<meta name="twitter:description" content="Roman publié par billets de blog" />
+		<meta name="twitter:card" content="summary" />
+		<meta name="twitter:site" content="@J.Forteroche" />
+		<meta name="twitter:title" content="Blog - Jean Forteroche" />
+		<meta name="twitter:image" content="" />
+
+		<link rel="icon" sizes="16x16" href="images/favicon.ico" />
+
+		<link href="styles/styles.css" type="text/css" rel="stylesheet" media="all" />
+	</head>
+
+	<body>
+		<header>
+            <h1>Bienvenue sur le blog !</h1>
+		</header>
+
+		
+		<footer>
+
+		</footer>
+
+	</body>
+</html>
--- public/index.php
+++ public/index.php
@@ -3,46 +3,18 @@
 require '../app/Autoloader.php';
 App\Autoloader::initiateAutoloader();
 
-?>
+$page = (!empty($_GET['p'])) ? $_GET['p'] : 'home';
 
-<!DOCTYPE html>
-<html lang="fr">
-	<head>
-		<meta charset="utf-8" />
+switch ($page)
+{
+	case 'home':
+		require '../pages/home.php';
+		break;
+	case 'post':
+		require '../pages/post.php';
+		break;
+	default:
+		echo 'Erreur : page non trouvée.';
+}
 
-		<title>Blog - Jean Forteroche</title>
 
-		<meta name="description" content="Roman publié par billets de blog" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-		<meta name="application-name" content="Billet simple pour l'Alaska" />
-		
-		<meta property="og:description" content="Roman publié par billets de blog" />
-		<meta property="og:title" content="Blog - Jean Forteroche" />
-		<meta property="og:site_name" content="Billet simple pour l'Alaska" />
-		<meta property="og:type" content="website" />
-		<meta property="og:url" content="http://maximeguillemot.com/formation/projet4/" />
-		<meta property="og:image" content="" />
-		
-		<meta name="twitter:description" content="Roman publié par billets de blog" />
-		<meta name="twitter:card" content="summary" />
-		<meta name="twitter:site" content="@J.Forteroche" />
-		<meta name="twitter:title" content="Blog - Jean Forteroche" />
-		<meta name="twitter:image" content="" />
-
-		<link rel="icon" sizes="16x16" href="images/favicon.ico" />
-
-		<link href="styles/styles.css" type="text/css" rel="stylesheet" media="all" />
-	</head>
-
-	<body>
-		<header>
-
-		</header>
-
-		
-		<footer>
-
-		</footer>
-
-	</body>
-</html>
